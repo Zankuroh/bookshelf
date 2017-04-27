c0d572146219472d5f704925948c679ec64f1e4a
Maxime B <maxime.bony@epitech.eu>
2017-04-27 15:15:57 -0700

    Vue d'un livre finie ! (et c'est beau)

--- app/src/main/java/com/eip/bookshelf/InfoBook.java
+++ app/src/main/java/com/eip/bookshelf/InfoBook.java
@@ -1,25 +1,23 @@
 package com.eip.bookshelf;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Locale;
 
 /**
@@ -28,7 +26,6 @@ import java.util.Locale;
 
 public class InfoBook extends AppCompatActivity
 {
-    private customAdapterCom _adapterCom;
     private ListView _lvCom;
     private ArrayList<ComAdapter> _modelListCom = new ArrayList<>();
 
@@ -43,49 +40,56 @@ public class InfoBook extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_info);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         _lvCom = (ListView) findViewById(R.id.LVCom);
         Bundle b = getIntent().getExtras();
+        HashMap<String, String> info = (HashMap<String, String>)b.getSerializable("info");
 
-        Log.d("test serializable", b.getSerializable("info").toString());
         setAdapters();
-//        moreDataBook();
+        moreDataBook(info);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setAdapters()
     {
-        _adapterCom = new customAdapterCom(this, _modelListCom);
+        customAdapterCom _adapterCom = new customAdapterCom(this, _modelListCom);
         _lvCom.setAdapter(_adapterCom);
-        //FAKE
+        //Todo: Récupérer les vrais commentaires :|
         _modelListCom.add(new ComAdapter("Maxime", "23/02/2016 à 13h42", "Super livre !"));
+        getTotalHeightofListView();
     }
 
-//    protected void moreDataBook(String title, List<Book> infos)
-//    {
-//        int i;
-//
-//        if (infos == null) {
-//            infos = tempHardTest();
-//        }
-//        if (infos != null) {
-//            for (i = 0; i < infos.size(); i++) {
-//                if (title.equals(infos.get(i).getTitre())) {
-//                    Book b = infos.get(i);
-//                    TextView tv = (TextView) findViewById(R.id.TVInfoBook);
-//                    TextView tvt = (TextView) findViewById(R.id.TVTitreBook);
-//                    TextView tvr = (TextView) findViewById(R.id.TVResum);
-//                    tvt.setText(b.getTitre());
-//                    tv.setText("Date de sortie : " + b.getDate());
-//                    tv.setText(tv.getText() + "\nAuteur : " + b.getAuteur());
-//                    tv.setText(tv.getText() + "\nGenre : " + b.getGenre());
-//                    tv.setText(tv.getText() + "\nNote : " + b.getNote().toString());
-//                    tvr.setText(b.getResum());
-//
-//                    /*ImageView iv = (ImageView) findViewById(R.id.IVBook);
-//                    iv.setImageBitmap(OptimizeBitmap.decodeSampledBitmapFromResource(getResources(), b.getImage(), 200, 200));*/
-//                }
-//            }
-//        }
-//    }
+    protected void moreDataBook(HashMap<String, String> info)
+    {
+        TextView tv = (TextView) findViewById(R.id.TVInfoBook);
+        TextView tvt = (TextView) findViewById(R.id.TVTitreBook);
+        TextView tvr = (TextView) findViewById(R.id.TVResum);
+        ImageView iv = (ImageView) findViewById(R.id.IVBook);
+
+        tvt.setText(info.get("title"));
+        tv.setText("Date de sortie : " + info.get("date"));
+        tv.setText(tv.getText() + "\nAuteur : " + info.get("author"));
+        tv.setText(tv.getText() + "\nGenre : " + info.get("genre"));
+        tv.setText(tv.getText() + "\nNote : " + info.get("note").toString());
+        tvr.setText(info.get("resume"));
+        if (info.get("picture") != null && info.get("picture") != "") {
+            Picasso.with(this).load(info.get("picture")).fit().into(iv);
+        }
+    }
 
     public void getTotalHeightofListView()
     {
@@ -115,7 +119,6 @@ public class InfoBook extends AppCompatActivity
         EditText et = (EditText) findViewById(R.id.ETCom);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH'h'mm", Locale.FRANCE);
         String currentDateandTime = sdf.format(new Date());
-        Log.i("sendCom", currentDateandTime);
         _modelListCom.add(new ComAdapter("Nicolas", currentDateandTime, et.getText().toString()));
         et.setText("");
         MainActivity.hideSoftKeyboard(InfoBook.this);
