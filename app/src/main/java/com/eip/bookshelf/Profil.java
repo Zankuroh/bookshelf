984a5ba8dbabf68028000f9aed84038d22fc8c02
Maxime B <maxime.bony@epitech.eu>
2017-04-28 21:06:43 -0700

    Ajout edition profil Petites modifs sur les view signin/signup Ajout fonction onclick sur view profil DONE !

--- app/src/main/java/com/eip/bookshelf/Profil.java
+++ app/src/main/java/com/eip/bookshelf/Profil.java
@@ -1,5 +1,6 @@
 package com.eip.bookshelf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,18 +11,39 @@ import android.view.ViewGroup;
  * Created by Maxime on 28/04/2017.
  */
 
-public class Profil extends Fragment
+public class Profil extends Fragment implements View.OnClickListener
 {
     public Profil()
     {
 
     }
 
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.profil, container, false);
+        View v = inflater.inflate(R.layout.profil, container, false);
+
+        v.findViewById(R.id.BEditProfil).setOnClickListener(this);
+        return v;
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.BEditProfil:
+                startActivity(new Intent(getActivity(), EditProfil.class));
+                break;
+            default:
+                break;
+        }
+    }
+
+//    private void onClickEditProfil()
+//    {
+//        startActivity(new Intent(getActivity(), EditProfil.class));
+//    }
+
     //Todo: Récupérer les info user dans la BDD
     //Todo: Rechercher un user ?
-    //Todo: Modifier les infos user (nouvelle vue ou popup)
 }
