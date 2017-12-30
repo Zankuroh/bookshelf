b49b6ce19a8228a9cf28615fb84195392e2f2309
TANGUC Sergen <sergen.tanguc@gmail.com>
2017-12-30 21:56:38 +0100

    qsdfqsdf

--- app/Http/Controllers/Api/SuggestionController.php
+++ app/Http/Controllers/Api/SuggestionController.php
@@ -245,6 +245,7 @@ class SuggestionController extends ApiController
 	 **/
 	private function fileGetContentsWithContext($url)
 	{
+		$url = urldecode($url);
 		Log::debug("FUCKING URL =" . $url);
 		$ch = curl_init();
 		$headers = array(
