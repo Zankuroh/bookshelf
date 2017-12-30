262961acd6daee3d765e4597bfa7897ad8903c79
TANGUC Sergen <sergen.tanguc@gmail.com>
2017-12-30 22:33:30 +0100

    debug mode guzzle

--- app/Http/Controllers/Api/SuggestionController.php
+++ app/Http/Controllers/Api/SuggestionController.php
@@ -253,6 +253,7 @@ class SuggestionController extends ApiController
 		
 		Log::debug("FUCKING URL =" . $url);
 		$res = $client->request('GET', $url , array(
+			'debug' => true,
 			'headers' => array(
 				'user-agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
 				'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
