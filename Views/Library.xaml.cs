d2ef8a38d39142ad1ef9299b258b67b7081a16e5
saillap <pierre.saillard@epitech.eu>
2018-01-01 01:38:46 +0100

    tri textuel

--- Views/Library.xaml.cs
+++ Views/Library.xaml.cs
@@ -23,14 +23,17 @@ namespace BookShelf
     /// </summary>
     public sealed partial class Library : Page
     {
-        List<ucBook> lib;
+        List<ucBook> lib = new List<ucBook>();
+        List<ucBook> Fav = new List<ucBook>();
+        List<ucBook> Read = new List<ucBook>();
+        List<ucBook> Unread = new List<ucBook>();
 
         public Library()
         {
             this.InitializeComponent();
         }
 
-        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        private async void Pivot_Loaded(object sender, RoutedEventArgs e)
         {
             clRequestAPI Req = new clRequestAPI("/api/book");
             string res = null;
@@ -49,10 +52,28 @@ namespace BookShelf
                     clBook bk = await clISBNsearch.SearchISBNclBook(it["isbn"].GetString());
                     bk.BookStatus = int.Parse(it["status_id"].GetString());
                     ucBook child = new ucBook(bk);
-                    wgrdLibrary.Children.Add(new ucBook(bk));
+                    lib.Add(child);
+                    wgrdAll.Children.Add(child);
+                    if (child.Book.BookStatus == 3)
+                    {
+                        ucBook b = child.Clone();
+                        Fav.Add(b);
+                        wgrdFav.Children.Add(b);
+                    }
+                    if (child.Book.BookStatus == 0)
+                    {
+                        ucBook b = child.Clone();
+                        Read.Add(b);
+                        wgrdRead.Children.Add(b);
+                    }
+                    if (child.Book.BookStatus == 1)
+                    {
+                        ucBook b = child.Clone();
+                        Unread.Add(b);
+                        wgrdUnRead.Children.Add(b);
+                    }
+
                 }
-                var b = wgrdLibrary.Children.ToList();
-                lib = b.Cast<ucBook>().ToList();
             }
             catch (Exception ex)
             {
@@ -62,13 +83,142 @@ namespace BookShelf
 
         private void txbxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            var filt = lib.Where(c => c.Book.BookData.VolumeInfo.Title.Contains(txbxSearch.Text));
-            wgrdLibrary.Children.Clear();
-            foreach (ucBook u in filt)
+            Func<ucBook, bool> mySearch = null;
+            try
             {
-                wgrdLibrary.Children.Add(u);
+                switch (cbbxAllSearch.SelectedIndex)
+                {
+                    case 0:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxAllSearch.Text);
+                        break;
+                    case 1:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Authors.FirstOrDefault().Contains(txbxAllSearch.Text);
+                        break;
+                    case 2:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Categories.FirstOrDefault().Contains(txbxAllSearch.Text);
+                        break;
+                    default:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxAllSearch.Text);
+                        break;
+                }
+                //var filt = lib.Where(c => c.Book.BookData.VolumeInfo.Title.Contains(txbxAllSearch.Text));
+                var filt = lib.Where(mySearch);
+                wgrdAll.Children.Clear();
+                foreach (ucBook u in filt)
+                {
+                    wgrdAll.Children.Add(u);
+                }
+                this.InitializeComponent();
             }
-            this.InitializeComponent();
+            catch (Exception ex)
+            {
+                clErrorHandling.ErrorMessage("txbxSearch_TextChanged(object sender, TextChangedEventArgs e)", ex);
+            }
+        }
+
+        private void txbxFavSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Func<ucBook, bool> mySearch = null;
+            try
+            {
+                switch (cbbxFavSearch.SelectedIndex)
+                {
+                    case 0:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxFavSearch.Text);
+                        break;
+                    case 1:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Authors.FirstOrDefault().Contains(txbxFavSearch.Text);
+                        break;
+                    case 2:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Categories.FirstOrDefault().Contains(txbxFavSearch.Text);
+                        break;
+                    default:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxFavSearch.Text);
+                        break;
+                }
+                //var filt = lib.Where(c => c.Book.BookData.VolumeInfo.Title.Contains(txbxFavSearch.Text));
+                var filt = Fav.Where(mySearch);
+                wgrdFav.Children.Clear();
+                foreach (ucBook u in filt)
+                {
+                    wgrdFav.Children.Add(u);
+                }
+                this.InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                clErrorHandling.ErrorMessage("txbxSearch_TextChanged(object sender, TextChangedEventArgs e)", ex);
+            }
+        }
+
+        private void txbxReadSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Func<ucBook, bool> mySearch = null;
+            try
+            {
+                switch (cbbxReadSearch.SelectedIndex)
+                {
+                    case 0:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxReadSearch.Text);
+                        break;
+                    case 1:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Authors.FirstOrDefault().Contains(txbxReadSearch.Text);
+                        break;
+                    case 2:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Categories.FirstOrDefault().Contains(txbxReadSearch.Text);
+                        break;
+                    default:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxReadSearch.Text);
+                        break;
+                }
+                //var filt = lib.Where(c => c.Book.BookData.VolumeInfo.Title.Contains(txbxReadSearch.Text));
+                var filt = Read.Where(mySearch);
+                wgrdRead.Children.Clear();
+                foreach (ucBook u in filt)
+                {
+                    wgrdRead.Children.Add(u);
+                }
+                this.InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                clErrorHandling.ErrorMessage("txbxReadSearch_TextChanged(object sender, TextChangedEventArgs e)", ex);
+            }
+        }
+
+        private void txbxUnreadSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Func<ucBook, bool> mySearch = null;
+            try
+            {
+                switch (cbbxUnreadSearch.SelectedIndex)
+                {
+                    case 0:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxUnReadSearch.Text);
+                        break;
+                    case 1:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Authors.FirstOrDefault().Contains(txbxUnReadSearch.Text);
+                        break;
+                    case 2:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Categories.FirstOrDefault().Contains(txbxUnReadSearch.Text);
+                        break;
+                    default:
+                        mySearch = (x) => x.Book.BookData.VolumeInfo.Title.Contains(txbxUnReadSearch.Text);
+                        break;
+                }
+                //var filt = lib.Where(c => c.Book.BookData.VolumeInfo.Title.Contains(txbxUnReadSearch.Text));
+                var filt = Unread.Where(mySearch);
+                wgrdUnRead.Children.Clear();
+                foreach (ucBook u in filt)
+                {
+                    wgrdUnRead.Children.Add(u);
+                }
+                this.InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+                clErrorHandling.ErrorMessage("txbxUnReadSearch_TextChanged(object sender, TextChangedEventArgs e)", ex);
+            }
         }
     }
 }
