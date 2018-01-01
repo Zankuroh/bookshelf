6b674ececaf3e77a5efe347cc4323afa352cd675
saillap <pierre.saillard@epitech.eu>
2018-01-01 02:49:39 +0100

    Implémenting Suggestions

---
+++ UserControls/ucSuggestions.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BookShelf
+{
+    public sealed partial class ucSuggestions : UserControl
+    {
+        string strISBN = null;
+        string strName = null;
+        string imgUrl = null;
+        public ucSuggestions()
+        {
+            this.InitializeComponent();
+        }
+        public ucSuggestions(string ISBN, string name, string img)
+        {
+            this.InitializeComponent();
+            try
+            {
+                strISBN = ISBN;
+                strName = name;
+                imgUrl = img;
+                txblNameBook.Text = strName;
+                Windows.UI.Xaml.Media.Imaging.BitmapImage bitmapImage = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+                Uri uri = new Uri(imgUrl);
+                bitmapImage.UriSource = uri;
+                imgThumb.Source = bitmapImage;
+            }
+            catch (Exception ex)
+            {
+                clErrorHandling.ErrorMessage("ucSuggestions", ex);
+            }
+        }
+
+        private async void ucSuggestion_Click(object sender, RoutedEventArgs e)
+        {
+            // The URI to launch
+            var uriBing = new Uri(@"http://api.duckduckgo.com/?q=\" + strISBN + "!amazon&t=BookShelf&format=json");
+            var success = await Windows.System.Launcher.LaunchUriAsync(uriBing);
+
+            if (success)
+            {
+                // URI launched
+            }
+            else
+            {
+                // URI launch failed
+            }
+        }
+    }
+}
--- Views/Suggestions.xaml.cs
+++ Views/Suggestions.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -30,6 +31,14 @@ namespace BookShelf
 
         private async void button_Click(object sender, RoutedEventArgs e)
         {
+            //Get
+            clRequestAPI Req2 = new clRequestAPI("/api/book/search/B01513ZIL6");
+            Req2.addAuthorization("Bearer", App.Token);
+            string res2 = await Req2.GetRequest();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
             clRequestAPI Req = new clRequestAPI("/api/suggestion");
             string res = null;
 
@@ -37,11 +46,29 @@ namespace BookShelf
             Req.addAuthorization("Bearer", App.Token);
 
             res = await Req.PostRequest("force_build=false", "application /x-www-form-urlencoded");
-
-            //Get
-            clRequestAPI Req2 = new clRequestAPI("/api/book/search/B01513ZIL6");
-            Req2.addAuthorization("Bearer", App.Token);
-            string res2 = await Req2.GetRequest();
+            JsonObject jsonRes;
+            JsonObject.TryParse(res, out jsonRes);
+            JsonObject j = jsonRes["data"].GetObject();
+            JsonArray t = j["latest_suggestions"].GetArray();
+            foreach (IJsonValue obj in t)
+            {
+                string it = obj.GetString();
+                clRequestAPI Req2 = new clRequestAPI("/api/book/search/" + it);
+                Req2.addAuthorization("Bearer", App.Token);
+                string res2 = await Req2.GetRequest();
+                JsonObject bk;
+                JsonObject.TryParse(res2, out bk);
+                JsonValueType type = bk["data"].ValueType;
+                JsonArray a = bk["data"].GetArray();
+                foreach (IJsonValue o in a)
+                {
+                    JsonObject suggest = o.GetObject();
+                    string title = suggest["book_title"].GetString();
+                    string img = suggest["book_picture_url"].GetString();
+                    ucSuggestions child = new ucSuggestions(it, title, img);
+                    wgrdAll.Children.Add(child);
+                }
+            }
         }
     }
 }
